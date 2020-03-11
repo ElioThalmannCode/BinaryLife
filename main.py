540d09f550b20fd13e303c3cf32560b54ad848f3
in19thel <in19thel@tfbern.com>
2020-03-11 09:46:18 +0100

    dfsddfdsf

--- main.py
+++ main.py
@@ -1,6 +1,19 @@
 import player_classes
 import possible_stats
 import random
+import time
+
+
+def menü()
+    print("""
+Was willst du tun?
+1   |   Karriere
+2   |   Aktivitäten
+3   |   Personen
+4   |   Besitztümer
+
+Nur Enter = nächstes Jahr
+""")
 
 
 def init_life():
@@ -11,19 +24,17 @@ def init_life():
     life = player_classes.yourself_class(random.choice(possible_stats.possible_male_first_names) if gender == 1 else random.choice(possible_stats.possible_female_first_names), random.choice(possible_stats.possible_last_names), gender, random.randrange(70,100), random.randrange(1,100), random.randrange(1,100), 0, 0)
     return life
 
-def socialization_birth():
+def hood_init():
     """
     your socializion you will have this your entire life
     """
-    hood = player_classes.hood(random.randrange(1, 5), random.randrange(1, 5))
-    friends
+    return player_classes.hood(random.randrange(1, 4), random.randrange(1, 4))
 
 def create_family(yourself):
     """
     creates you family,
     """
     #list with all members
-    family = []
     #mom
     mom = player_classes.npc(random.choice(possible_stats.possible_female_first_names),yourself.last_name, 2, random.randrange(20,30), random.randrange(70,100),0,random.randrange(1,5), True, True, random.randrange(1,100), random.randrange(1,100))
     #dad
@@ -37,14 +48,16 @@ def create_family(yourself):
     for x in range(random.randrange(0,3)):
         brother = player_classes.npc(random.choice(possible_stats.possible_male_first_names),yourself.last_name, 1, random.randrange(2,4), random.randrange(70,100),0,random.randrange(1,5), True, True, random.randrange(1,100), random.randrange(1,100))
         brothers.append(brother)
-    return sisters, brothers
+    family = [sisters, brothers, mom, dad]
+    print(family)
+    return [sisters, brothers, mom, dad]
 
 
 def start():
     yourself = init_life()
     print(yourself)
-    sisters, brothers = create_family(yourself)
-    print(sisters, brothers)
+    family = create_family(yourself)
+    hood = hood_init()
     print(f"""
 Hallo und willkommen zu Bitlife.
 
@@ -60,6 +73,15 @@ Deine Inteligenz:
 Dein Aussehen:
 |{(yourself.looks // 5) * "█"}|{yourself.looks}/100
   """)
+    time.sleep(3)
+    print(f"""
+Dein Name ist {yourself.first_name} {yourself.last_name}. Du hast {len(family[0])} Schwester/n und {len(family[1])} Bruder/Brüder.
+Deine Mutter heisst {family[2].first_name} {family[2].last_name} und dein Vater heisst {family[3].first_name} {family[3].last_name}.
+Deine Familie lebt in einer {'sehr armen' if hood.save == 1 else ('normal wohlhabenden' if hood.save == 2 else 'sehr armen')} Gegend mit einem {'sehr gutem' if hood.education == 1 else ('mittelmässigen' if hood.education == 2 else 'sehr schlechtem')} Schulsystem.
+    """)
+    input("Enter drücken zum starten")
+    hallo()
+
 
 def main():
     """
@@ -70,4 +92,3 @@ def main():
 
 if __name__ == "__main__":
     main()
-
