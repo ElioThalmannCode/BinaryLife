a5cbe4e0b6999a0e4f96f1529798922a698fde2d
in19thel <in19thel@tfbern.com>
2020-03-25 09:55:56 +0100

    Kivi init

--- GUI.py
+++ GUI.py
@@ -1,12 +1,52 @@
-from tkinter import *
-from functions import say_hello
-window = Tk()
-window.title("BinaryLife")
-window.geometry('800x1200')     
-lbl = Label(window)
-lbl.grid(column=1, row=0)
-def clicked():
-    lbl.configure(text=say_hello())
-btn = Button(window, text="Start", command=clicked)
-btn.grid(column=0, row=0)
-window.mainloop()
+import kivy                                 
+from kivy.app import App                    
+from kivy.uix.label import Label            
+from kivy.uix.gridlayout import GridLayout  
+from kivy.uix.textinput import TextInput    
+from kivy.uix.button import Button          
+from kivy.uix.widget import Widget          
+from kivy.properties import ObjectProperty  
+from pydblite import *                      
+from kivy.factory import Factory            
+from kivy.uix.screenmanager import ScreenManager, Screen
+from kivy.lang import Builder
+
+Builder.load_string("""
+<MenuScreen>:
+
+    BoxLayout:
+        orientation: 'vertical'
+        Image:
+            source: 'mylogo.png'
+            size: self.texture_size
+        Button:
+            text: 'Start'
+            on_press: root.manager.current = 'start'
+
+
+<StartScreen>:
+    BoxLayout:
+        Button:
+            text: 'Back to menu'
+            on_press: root.manager.current = 'menu'
+""")
+
+# Declare both screens
+class MenuScreen(Screen):
+    pass
+
+class StartScreen(Screen):
+    pass
+
+# Create the screen manager
+sm = ScreenManager()
+sm.add_widget(MenuScreen(name='menu'))
+sm.add_widget(StartScreen(name='start'))
+
+class TestApp(App):
+
+    def build(self):
+        return sm
+
+if __name__ == '__main__':
+    TestApp().run()
